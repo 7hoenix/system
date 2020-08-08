ddd0065ca2f52150b11ab759b3afe86d2d55f977
J 7hoenix <jpholzmann@gmail.com>
2020-08-08 13:35:14 -0500

    Support bashrc in Linux.

--- linux/install.bash
+++ linux/install.bash
@@ -1,7 +1,9 @@
+set -e
+
 pushd "$(dirname "${0}")/.." > /dev/null
   SYSTEM_ROOT="$(pwd)"
 popd > /dev/null
 
 source $SYSTEM_ROOT/universal/install.bash
 
-_install
+_install "linux"
--- universal/install.bash
+++ universal/install.bash
@@ -1,3 +1,19 @@
 function _install {
-  ln -s $SYSTEM_ROOT/universal/dotfiles/git/gitconfig ~/.gitconfig
+  if [[ $# -ne 1 ]]; then
+    echo "Must specify operating system."
+    echo "e.g. linux | mac"
+    return 1
+  fi
+  local os=$1
+  declare -A configs
+  configs=(
+      ['.gitconfig']="$SYSTEM_ROOT/universal/dotfiles/git/gitconfig"
+      ['.bashrc']="$SYSTEM_ROOT/$os/dotfiles/bash/bashrc"
+  )
+
+  for target in "${!configs[@]}"
+  do
+    rm ~/$target 2> /dev/null || true
+    ln -s ${configs[$target]} ~/$target
+  done
 }
