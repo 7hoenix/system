09d3dcb1562d8f959347d74d503685b05cac7904
J 7hoenix <jpholzmann@gmail.com>
2020-08-10 07:34:43 -0500

    Hardcode SYSTEM_ROOT as the bashrc wasn't being executed as a script

--- universal/aliases.bash
+++ universal/aliases.bash
@@ -1,1 +1,13 @@
 alias pdocs="less $SYSTEM_ROOT/pdocs/pdocs"
+
+alias gpfwl='git push --force-with-lease'
+alias gcap='git commit --amend --no-edit && git push --force-with-lease'
+alias gs='git status'
+alias gdc='git diff --cached'
+alias ga='git add'
+alias gc='git commit'
+alias gl='git log'
+
+alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
+
+alias 7copy="history 2 | head -n1 | cut -d' ' -f 5- | pbcopy"
